e3c0a8641e2f4d0d201726866480dd2db1106cfc
longluo <calmbody@163.com>
2021-12-28 16:19:06 +0800

    add the droidui droidutils module and the demo show.

--- app/src/main/java/com/longluo/android/App.java
+++ app/src/main/java/com/longluo/android/App.java
@@ -1,22 +1,96 @@
 package com.longluo.android;
 
-import android.app.Application;
-import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
-import com.longluo.android.db.greendao.DbCore;
-import com.umeng.commonsdk.UMConfigure;
+import androidx.appcompat.app.AlertDialog;
 
+import com.longluo.android.bean.User;
 
-public class App extends Application {
+import java.io.File;
+
+import io.github.longluo.baseframework.BaseApp;
+import io.github.longluo.baseframework.BaseFrameworkSettings;
+import io.github.longluo.baseframework.interfaces.OnBugReportListener;
+import io.github.longluo.baseframework.interfaces.OnSDKInitializedCallBack;
+import io.github.longluo.baseframework.util.AppManager;
+import io.github.longluo.baseframework.util.SettingsUtil;
+
+public class App extends BaseApp<App> {
 
     @Override
-    public void onCreate() {
-        super.onCreate();
+    public void init() {
+        setOnSDKInitializedCallBack(new OnSDKInitializedCallBack() {
+            @Override
+            public void onInitialized() {
+                log("onInitialized: ");
+                Toast.makeText(me, "SDK已加载完毕", Toast.LENGTH_LONG).show();
+            }
+        });
 
-        DbCore.init(this);
-        DbCore.enableQueryBuilderLog();
+        setOnCrashListener(new OnBugReportListener() {
+            @Override
+            public boolean onCrash(Exception e, final File crashLogFile) {
+                if (AppManager.getInstance().getActiveActivity() == null || !AppManager.getInstance().getActiveActivity().isActive) {
+                    return false;
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(AppManager.getInstance().getActiveActivity());
+                builder.setTitle("Ops！发生了一次崩溃！");
+                builder.setMessage("您是否愿意帮助我们改进程序以修复此Bug？");
+                builder.setPositiveButton("愿意", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        toast("请对file进行处理：" + crashLogFile.getAbsolutePath());
+                    }
+                });
+                builder.setNegativeButton("不了", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-        UMConfigure.preInit(this,"61bc361fe014255fcbbb8876","Mine");
+                    }
+                });
+                builder.setCancelable(false);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return false;
+            }
+        });
+
+//        Bitmap bm = null;       //随便写的 Demo
+//        App.cache.set("key","value");
+//        App.cache.set("bitmap",bm);
+//        App.cache.clean();
+
+        User user = new User("张三", 18, "192.168.1.1");
+        App.user.set("userInfo", user);
     }
 
+    public static USER user = new USER();
+
+    public static class USER extends SettingsUtil {
+
+        public USER() {
+            super("user");
+        }
+    }
+
+    public static CACHE cache;
+
+    public static class CACHE extends SettingsUtil {
+
+        public CACHE() {
+            super("cache");
+        }
+    }
+
+    @Override
+    public void initSDKs() {
+        BaseFrameworkSettings.DEBUGMODE = true;
+        BaseFrameworkSettings.BETA_PLAN = true;
+        try {
+            Thread.sleep(8000);
+        } catch (Exception e) {
+        }
+    }
 }
