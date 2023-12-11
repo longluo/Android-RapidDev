0b6e1ec059cc1f8b3cb3d03ccf57c91fd6ef9bc7
longluo <luolong0226@163.com>
2023-12-11 11:04:29 +0800

    add the fastclick button

--- app/src/main/java/me/longluo/android/ui/fragment/MessageFragment.java
+++ app/src/main/java/me/longluo/android/ui/fragment/MessageFragment.java
@@ -1,12 +1,16 @@
 package me.longluo.android.ui.fragment;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.hjq.permissions.Permission;
 import com.hjq.permissions.XXPermissions;
+
+import io.github.longluo.util.ToastUtils;
+import io.github.longluo.util.helpers.OnClickEvent;
 import me.longluo.android.R;
 import me.longluo.android.aop.Permissions;
 import me.longluo.android.aop.SingleClick;
@@ -20,6 +24,8 @@ public final class MessageFragment extends TitleBarFragment<HomeActivity> {
 
     private ImageView mImageView;
 
+    private Button mBtnFastClick;
+
     public static MessageFragment newInstance() {
         return new MessageFragment();
     }
@@ -32,14 +38,18 @@ public final class MessageFragment extends TitleBarFragment<HomeActivity> {
     @Override
     protected void initView() {
         mImageView = findViewById(R.id.iv_message_image);
+
         setOnClickListener(R.id.btn_message_image1, R.id.btn_message_image2, R.id.btn_message_image3,
                 R.id.btn_message_toast, R.id.btn_message_permission, R.id.btn_message_setting,
                 R.id.btn_message_black, R.id.btn_message_white, R.id.btn_message_tab);
+
+        mBtnFastClick = findViewById(R.id.btn_fast_click);
     }
 
     @Override
     protected void initData() {
 
+        mBtnFastClick.setOnClickListener(new MySingleClickListener(1500));
     }
 
     @Override
@@ -104,11 +114,24 @@ public final class MessageFragment extends TitleBarFragment<HomeActivity> {
         } else if (viewId == R.id.btn_message_tab) {
 
             HomeActivity.start(getActivity(), HomeFragment.class);
+
         }
     }
 
     @Permissions(Permission.CAMERA)
     private void requestPermission() {
         toast("获取摄像头权限成功");
+    }
+
+    public class MySingleClickListener extends OnClickEvent {
+
+        public MySingleClickListener(long delay) {
+            super(delay);
+        }
+
+        @Override
+        public void singleClick(View v) {
+            ToastUtils.showToast(getContext(), "A Click");
+        }
     }
 }
